4580ee7945707e118d0aab004a666572291c48e8
Chinnathambi_Chandran <166941286+chinnathambi1406@users.noreply.github.com>
2025-12-19 20:55:43 +0530

    Time: 1 ms (100%), Space: 43.9 MB (26.15%) - LeetHub

---
+++ 2908-minimum-sum-of-mountain-triplets-i/2908-minimum-sum-of-mountain-triplets-i.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minimumSum(int[] nums) {
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                for(int k=j+1;k<nums.length;k++)
+                {
+                    if(nums[i]<nums[j]&&nums[k]<nums[j])
+                    {
+                        int sum=nums[i]+nums[j]+nums[k];
+                         min=Math.min(sum,min);
+                    }
+                }
+            }
+        }
+        return (min==Integer.MAX_VALUE)?-1:min;
+    }
+}
